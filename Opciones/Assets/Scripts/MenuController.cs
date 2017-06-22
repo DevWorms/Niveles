7ff60e2d20301a997774ff40abd320c1afdb8957
Juan Reyes <jj_reyesc@hotmail.com>
2017-06-22 14:32:49 -0500

    Menu Opciones- Solo Musica de Fondo

---
+++ Opciones/Assets/Scripts/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MenuController : MonoBehaviour {
+
+    public GameObject Inicial;
+    public GameObject Opciones;
+    public Slider SfxSlider;
+    public Slider MusicSlider;
+    public AudioSource Music;
+
+
+	void Start () {
+
+        PlayerPrefs.SetFloat("MusicVolume",Music.volume);
+	}
+    public void OptionsMenu() {
+        Inicial.SetActive(false);
+        Opciones.SetActive(true);
+    }
+	
+	public void AcceptChanges (){
+        PlayerPrefs.SetFloat("MusicVolume", MusicSlider.value);
+        Inicial.SetActive(true);
+        Opciones.SetActive(false);
+    }
+    public void CancelChanges()
+    {
+        MusicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        Inicial.SetActive(true);
+        Opciones.SetActive(false);
+    }
+    public void SetMusicVolume(float value) {
+        Music.volume = MusicSlider.value;
+    }
+}
