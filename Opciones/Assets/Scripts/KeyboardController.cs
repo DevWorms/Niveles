6c3af67a13e1aaa6b0d491d81661257076cc7ed7
Gustavo Gama <beattguss@gmail.com>
2017-06-24 05:29:08 -0500

    Falta la tecla shift y el teclado numerico
    ya funciona pero me falta hacer lo otro :B

---
+++ Opciones/Assets/Scripts/KeyboardController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class KeyboardController : MonoBehaviour {
+    [SerializeField]
+    Text inputField;
+    string inputString;
+    public void ButtomPressed()
+    {
+        Debug.Log(EventSystem.current.currentSelectedGameObject.name);
+        string ButtonValue = EventSystem.current.currentSelectedGameObject.name;
+        inputString += ButtonValue;
+        inputField.text = inputString;
+    }
+    public void ActionPressed()
+    {
+        string action = EventSystem.current.currentSelectedGameObject.name;
+        if (action == "Space")
+        {
+            inputString += " ";
+        }
+        else if (action == "Delete")
+        {
+            inputString = inputString.Substring(0, inputString.Length - 1);
+        }
+        inputField.text = inputString;
+    }
+
+
+}
+
