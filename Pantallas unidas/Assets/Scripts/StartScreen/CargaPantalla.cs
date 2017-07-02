0f18ac663725cfe28f310b5a36237b403bbf4bf3
Ariel Israel de la O Zamorano <ariel_dlz@live.com>
2017-07-02 11:51:20 -0500

    Proyecto nuevo con todas las pantallas integradas

---
+++ Pantallas unidas/Assets/Scripts/StartScreen/CargaPantalla.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CargaPantalla : MonoBehaviour {
+
+	public void CargaEscena(int Escena)
+    {
+        Application.LoadLevel(Escena);
+    }
+}
