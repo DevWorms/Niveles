d2c37a0a292fd7f5bebbf400d0294d313f7e1a29
MartinGarCar <martin_garcia.775@hotmail.com>
2017-07-29 17:14:04 -0500

    Galletita

---
+++ BM Personajes/Assets/Scripts/Galletita/GalletitaAtaque.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalletitaAtaque : MonoBehaviour {
+    public Animator Animacion;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Animacion.SetBool("GalletitaAttack", true);
+        }
+        else
+        {
+            Animacion.SetBool("GalletitaAttack", false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Animacion.SetBool("GalletitaAttack", false);
+        }
+    }
+}
---
+++ BM Personajes/Assets/Scripts/Galletita/GalletitaCamina.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalletitaCamina : MonoBehaviour
+{
+    public GameObject StartPoint;
+    public GameObject EndPoint;
+    private AreaEffector2D galletita_T;
+    public float EnemySpeed;
+    private bool GoRight;
+    public Animator galletita;
+    public Animator bagman;
+
+    // Use this for initialization
+    void Start ()
+    {
+        galletita_T = GetComponent<AreaEffector2D>();
+        if (!GoRight)
+        {
+            transform.position = StartPoint.transform.position;
+        }
+        else
+        {
+            transform.position = EndPoint.transform.position;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (bagman.GetBool("GalletitaAttack") && GoRight)
+        {
+            galletita.SetBool("atacar", true);
+            galletita_T.enabled = true;
+
+        }
+        else
+        {
+            galletita.SetBool("atacar", false);
+            galletita_T.enabled = false;
+            if (!GoRight)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, EndPoint.transform.position, EnemySpeed * Time.deltaTime);
+                if (transform.position == EndPoint.transform.position)
+                {
+                    GoRight = true;
+                    GetComponent<SpriteRenderer>().flipX = true;
+                }
+            }
+            if (GoRight)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, StartPoint.transform.position, EnemySpeed * Time.deltaTime);
+                if (transform.position == StartPoint.transform.position)
+                {
+                    GoRight = false;
+                    GetComponent<SpriteRenderer>().flipX = false;
+                }
+            }
+        }
+    }
+}
